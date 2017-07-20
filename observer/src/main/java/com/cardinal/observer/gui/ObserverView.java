0c087ece0d8b528148d34c3a3d22185372d24b24
maperez <maperez@cardinalsystems.com.ar>
2017-07-20 15:24:51 -0300

    Implemented a test GUI using the Swing observer/observable objects.

---
+++ observer/src/main/java/com/cardinal/observer/gui/ObserverView.java
@@ -0,0 +1,45 @@
+package com.cardinal.observer.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by maperez on 20/7/2017.
+ */
+public class ObserverView {
+    JFrame jFrame;
+
+    public static void main(String[] args) {
+        ObserverView observerView = new ObserverView();
+        observerView.run();
+    }
+
+    public void run() {
+        jFrame = new JFrame();
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new VeryGoodListener());
+        button.addActionListener(new EvilListener());
+        jFrame.getContentPane().add(BorderLayout.CENTER, button);
+        jFrame.setSize(300,300);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    class VeryGoodListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Yes, why not?");
+        }
+    }
+
+    class EvilListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Your world is gonna blow, do it!!");
+        }
+    }
+}
